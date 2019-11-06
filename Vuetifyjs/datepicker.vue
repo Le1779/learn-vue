cca402f71ac7ed2dbbe9269a1a1a28d7928688f0
kevinle <le8419951779@gmail.com>
2019-11-06 17:16:23 +0800

    add vuetify datepicker

---
+++ Vuetifyjs/datepicker.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-menu v-model="menu2" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="290px">
+        <template v-slot:activator="{ on }">
+            <v-text-field v-model="date" label="Picker without buttons" readonly v-on="on"></v-text-field>
+        </template>
+        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+    </v-menu>
+</template>
+
+
+<script>
+    module.exports = {
+        data: () => ({
+            date: new Date().toISOString().substr(0, 10),
+            menu: false,
+            modal: false,
+            menu2: false,
+        }),
+    }
+
+</script>
