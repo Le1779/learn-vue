73b1370a1e7650d918b3aaaddc0e69ddb954f697
kevinle <le8419951779@gmail.com>
2019-11-07 14:43:17 +0800

    從datatable拆出dialog

---
+++ Vuetifyjs/datatable-dialog.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-dialog v-model="dialog_model.show" max-width="500px">
+        <v-card>
+            <v-card-title>
+                <span class="headline">{{ form_title }}</span>
+            </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="edited_item.name" label="Dessert name"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="edited_item.calories" label="Calories"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="edited_item.fat" label="Fat (g)"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="edited_item.carbs" label="Carbs (g)"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="edited_item.protein" label="Protein (g)"></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    module.exports = {
+        props: ["dialog_model", "edited_item", "form_title"],
+        methods: {
+            close() {
+                this.dialog_model.show = false;
+            },
+            save() {
+                this.$emit('dialog_data', false)
+                this.close();
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+
+
+</style>
--- Vuetifyjs/datatable.vue
+++ Vuetifyjs/datatable.vue
@@ -6,44 +6,8 @@
                     <v-toolbar-title>My CRUD</v-toolbar-title>
                     <v-divider class="mx-4" inset vertical></v-divider>
                     <v-spacer></v-spacer>
-                    <v-dialog v-model="dialog" max-width="500px">
-                        <template v-slot:activator="{ on }">
-                            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
-                        </template>
-                        <v-card>
-                            <v-card-title>
-                                <span class="headline">{{ formTitle }}</span>
-                            </v-card-title>
+                    <v-btn color="primary" dark class="mb-2" @click="createItem">New Item</v-btn>
 
-                            <v-card-text>
-                                <v-container>
-                                    <v-row>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" sm="6" md="4">
-                                            <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-card-text>
-
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
                 </v-toolbar>
             </template>
             <template v-slot:item.action="{ item }">
@@ -58,6 +22,7 @@
                 <v-btn color="primary" @click="initialize">Reset</v-btn>
             </template>
         </v-data-table>
+        <datatable-dialog :dialog_model="dialog_model" :edited_item="editedItem" :form_title="'formTitle'" @dialog_data="save"></datatable-dialog>
     </v-card>
 </template>
 
@@ -65,6 +30,9 @@
 <script>
     module.exports = {
         data: () => ({
+            dialog_model: {
+                show: false,
+            },
             dialog: false,
             headers: [{
                     text: 'Dessert (100g serving)',
@@ -249,8 +217,47 @@
                 
                 this.totalDesserts = this.desserts.length;
                 this.loading = false;
-            }
+            },
+
+            editItem(item) {
+                this.editedIndex = this.desserts.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog_model.show = true
+            },
+
+            deleteItem(item) {
+                const index = this.desserts.indexOf(item)
+                confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+            },
+
+            createItem() {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+                this.dialog_model.show = true
+            },
+
+            close() {
+                this.dialog_model.show = false
+                setTimeout(() => {
+                    this.editedItem = Object.assign({}, this.defaultItem)
+                    this.editedIndex = -1
+                }, 300)
+            },
+
+            save() {
+                console.log('save');
+                if (this.editedIndex > -1) {
+                    Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                } else {
+                    this.desserts.push(this.editedItem)
+                }
+                this.close()
+            },
         },
+
+        components: {
+            'datatable-dialog': httpVueLoader('datatable-dialog.vue')
+        }
     }
 
 </script>
