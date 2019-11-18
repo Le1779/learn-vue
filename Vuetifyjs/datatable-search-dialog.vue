917916a9ce8adb6247f97a09552da0c5140e2d3e
kevinle <le8419951779@gmail.com>
2019-11-18 15:42:51 +0800

    add search dialog

---
+++ Vuetifyjs/datatable-search-dialog.vue
@@ -0,0 +1,75 @@
+<template>
+    <v-dialog v-model="dialog_model.show" persistent max-width="500px">
+        <v-card>
+            <v-card-title>
+                <span class="headline">{{ dialog_model.title  }}</span>
+            </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="search_item.name" label="Dessert name"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="search_item.calories" label="Calories"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="search_item.fat" label="Fat (g)"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="search_item.carbs" label="Carbs (g)"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="search_item.protein" label="Protein (g)"></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" text :loading="loading" :disabled="loading" @click="save">Search</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    let viewModel = {
+        loading: false
+    };
+
+    module.exports = {
+        props: ["dialog_model", "search_item", "form_title"],
+        data: function() {
+            return viewModel
+        },
+        methods: {
+            close() {
+                this.dialog_model.show = false;
+            },
+            save() {
+                this.loading = true;
+                this.$emit('action', false)
+            }
+        },
+        watch: {
+            dialog_model: {
+                handler(val) {
+                    if(val.show){
+                        this.loading = false;
+                    }
+                },
+                deep: true
+            },
+        }
+    }
+
+</script>
+
+<style scoped>
+
+
+</style>
