88bc1e98ccb53e3466ae5063dbb1df3742f77763
kevinle <le8419951779@gmail.com>
2020-01-07 15:11:23 +0800

    完成搜尋dialog

--- VegetableStore/Product/dialog-search-product.vue
+++ VegetableStore/Product/dialog-search-product.vue
@@ -7,22 +7,28 @@
 
             <v-card-text>
                 <v-container>
-                   {{ pickedDate }}
                     <v-row>
                         <v-col cols="12" sm="6">
                             <v-text-field v-model="dialog_model.item.Name" label="商品名稱"></v-text-field>
                         </v-col>
                         <v-col cols="12" sm="6">
-                            <v-dialog ref="dialog" v-model="menu" :return-value.sync="pickedDate" persistent width="290px">
+                            <v-select v-model="dialog_model.item.IsInStock" :items="inStockItems" item-text="text" item-value="value" label="是否上架"></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-menu v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px" min-width="290px">
                                 <template v-slot:activator="{ on }">
-                                    <v-text-field v-model="showPickedDate" label="Picker in dialog" prepend-icon="event" readonly v-on="on"></v-text-field>
+                                    <v-text-field v-model="dialog_model.item.CreateDate" label="從" v-on="on"></v-text-field>
                                 </template>
-                                <v-date-picker v-model="pickedDate" scrollable range>
-                                    <v-spacer></v-spacer>
-                                    <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-                                    <v-btn text color="primary" @click="$refs.dialog.save(pickedDate)">OK</v-btn>
-                                </v-date-picker>
-                            </v-dialog>
+                                <v-date-picker v-model="dialog_model.item.CreateDate" no-title @input="menu = false" :max="createDateEnd"></v-date-picker>
+                            </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-menu v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px" min-width="290px">
+                                <template v-slot:activator="{ on }">
+                                    <v-text-field v-model="createDateEnd" label="到" v-on="on"></v-text-field>
+                                </template>
+                                <v-date-picker v-model="createDateEnd" no-title @input="menu2 = false" :min="dialog_model.item.CreateDate"></v-date-picker>
+                            </v-menu>
                         </v-col>
                         <v-col cols="12" sm="6">
                             <v-text-field v-model="dialog_model.item.Price" label="售價"></v-text-field>
@@ -43,10 +49,10 @@
                 </v-container>
             </v-card-text>
 
-           <v-card-actions class="px-7 py-0">
+            <v-card-actions class="px-7 py-4">
                 <v-spacer></v-spacer>
-                <v-btn color="grey darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" class="white--text" :loading="dialog_model.loading" :disabled="dialog_model.loading" @click="save">Search</v-btn>
+                <v-btn color="grey darken-1" text @click="close">取消</v-btn>
+                <v-btn color="blue darken-1" class="white--text" :loading="dialog_model.loading" :disabled="dialog_model.loading" @click="save">搜尋</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -58,12 +64,23 @@
 
         data: () => ({
             menu: false,
-            pickedDate: [],
+            menu2: false,
+            createDateEnd: '',
             title: '搜尋商品',
             unitItems: ['個', '根', '箱', '袋'],
             areaItems: ['全區', '北區', '中區', '南區', '東區'],
-            inStockItems: [
-                {text: '', value: null}
+            inStockItems: [{
+                    text: '無',
+                    value: -1
+                },
+                {
+                    text: '是',
+                    value: 1
+                },
+                {
+                    text: '否',
+                    value: 0
+                }
             ]
         }),
 
@@ -76,13 +93,13 @@
                 this.$emit('action', false)
             }
         },
-        
+
         computed: {
             showPickedDate() {
                 return this.pickedDate.length <= 1 ? null : this.pickedDate[0] + "~" + this.pickedDate[1]
             },
         },
-        
+
         watch: {
             dialog_model: {
                 handler(val) {
