fa979add8f9d4a2bba46013cd7d209265567711d
kevinle <le8419951779@gmail.com>
2020-05-24 18:49:33 +0800

    add guide

---
+++ Guide/guide.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <input type="text" v-model="message" style="width:100%;" />
+        Message: {{message}}
+        
+        <div style="margin:24px;">
+            {{time}}
+        </div>
+        
+        現在{{time}} 有人說{{message}}
+    </div>
+</template>
+
+<script scoped>
+    module.exports = {
+        data: () => ({
+            time: "",
+            message: ""
+        }),
+
+        created() {
+            this.getTime();
+            setInterval(() => {
+                this.getTime();
+            }, 1000)
+        },
+
+        methods: {
+            getTime() {
+                this.time = String(new Date())
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+
+</style>
