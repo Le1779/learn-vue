0001e70b1844785fbe7925344f7c8097a767c3f3
kevinle <le8419951779@gmail.com>
2021-03-08 17:39:13 +0800

    [ReleaseFont] [Add] font list

--- ReleaseFont/layout-main.vue
+++ ReleaseFont/layout-main.vue
@@ -6,20 +6,21 @@ Created by Kevin Le on 2021/3/4.
 <template>
     <div class="main">
         <div class="header">
-            <loading-button v-if="displayFirstPage && excelFile && fontFile" :model="loadingButtonModel" class="forward" @action="onForwardButtonClick"></loading-button>
+            <loading-button v-if="isUploadAllFontFile" :model="loadingButtonModel" class="forward" @action="onForwardButtonClick"></loading-button>
             <div v-if="!displayFirstPage" class="back button material-icons" @click="onBackButtonClick">keyboard_arrow_left</div>
             <div class="error-message">{{errorMessage}}</div>
-            <div class="loader" :class="{active: excelFile && fontFile && loading}"></div>
         </div>
         <div class="content">
             <div v-if="displayFirstPage" class="first-page">
-               <upload-file-block :model="uploadFileBlockModel" @tirgger="tirggerExcelFile" class="upload-file-block left"></upload-file-block>
-                
-                <div class="upload-file-block" :class="{active: fontFile && !loadingButtonModel.loading}">
-                    <div class="material-icons">title</div>
-                    <div v-if="!fontFile" class="text">上傳 Font</div>
-                    <div v-else class="selected-file-name">{{fontFile.name}}</div>
-                    <input class="file" type="file" @change="tirggerFontFile($event)" :disabled="loadingButtonModel.loading">
+                <upload-file-block :model="uploadFileBlockModel" @tirgger="tirggerExcelFile" class="upload-excel"></upload-file-block>
+                <div class="font-list">
+                    <div v-for="(item, i) in fontList" class="font-list-item" :class="{active: item.file && !item.notTheSame}">
+                        <div class="font-list-item-icon material-icons">task</div>
+                        <div class="font-list-item-font-name">{{item.Font_Name}}</div>
+                        <div class="font-list-item-font-filename">{{item.Font_FileName}}.{{item.Font_FileType}}</div>
+                        <div v-if="item.file && item.notTheSame" class="font-list-item-not-the-same-filename">上傳的檔案名稱不符合</div>
+                        <input class="font-list-item-file" type="file" :accept="'.' + item.Font_FileType" @change="tirggerFontFile($event, item)">
+                    </div>
                 </div>
             </div>
             <div v-else class="second-page">
@@ -49,16 +50,28 @@ Created by Kevin Le on 2021/3/4.
             displayFirstPage: true,
             errorMessage: "",
             loading: false,
-            excelFile: {
-                name: "DFKing.ttf"
-            },
-            fontFile: {
-                name: "DFKing.ttf"
-            },
+            excelFile: null,
+            fontList: []
         }),
 
         watch: {
 
+        },
+        
+        computed: {
+            isUploadAllFontFile() {
+                if (!this.excelFile) {
+                    return false;
+                }
+                
+                for (var i = 0; i < this.fontList.length; i++) {
+                    if (!this.fontList[i].file || this.fontList[i].notTheSame) {
+                        return false;
+                    }
+                }
+                
+                return true;
+            }
         },
 
         mounted() {},
@@ -72,13 +85,51 @@ Created by Kevin Le on 2021/3/4.
 
         methods: {
             tirggerExcelFile(event) {
+                var self = this;
                 this.excelFile = event.target.files[0];
                 console.log(this.excelFile);
+                readFile(this.excelFile);
+
+                function readFile(file) {
+                    var reader = new FileReader();
+                    var name = file.name;
+
+                    reader.onload = function(e) {
+                        var data = e.target.result;
+                        getFontList(data);
+                        console.log(self.fontList)
+                    };
+
+                    reader.readAsBinaryString(file);
+                }
+
+                function getFontList(data) {
+                    var workbook = XLSX.read(data, {
+                        type: 'binary'
+                    });
+
+                    self.fontList = [];
+                    workbook.SheetNames.forEach(function(sheetName) {
+                        var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                        if (roa.length > 0) {
+                            for (var i = 0; i < roa.length; i++) {
+                                roa[i].notTheSame = false;
+                                roa[i].file = null;
+                                self.fontList.push(roa[i]);
+                            }
+                        }
+                    });
+                }
             },
 
-            tirggerFontFile(event) {
-                this.fontFile = event.target.files[0];
-                console.log(this.fontFile);
+            tirggerFontFile(event, item) {
+                item.file = event.target.files[0];
+                console.log(item.file);
+                if (!item.file) {
+                    return;
+                }
+                
+                item.notTheSame = item.file.name != item.Font_FileName + "." + item.Font_FileType
             },
 
             onForwardButtonClick() {
@@ -101,12 +152,12 @@ Created by Kevin Le on 2021/3/4.
             uploadAndVerifyFile() {
                 this.uploadFileBlockModel.disabled = true;
                 var self = this;
-                setTimeout(()=>{
+                setTimeout(() => {
                     self.loadingButtonModel.loading = false;
-                this.uploadFileBlockModel.disabled = false;
+                    this.uploadFileBlockModel.disabled = false;
                     self.goToSecondPage();
                 }, 1000)
-                
+
             }
         },
     }
@@ -120,10 +171,15 @@ Created by Kevin Le on 2021/3/4.
         border: #AAA 1px solid;
         border-radius: 5px;
         margin: 48px 24px;
+        padding: 0 24px;
     }
 
     .header {
         display: block;
+    }
+
+    .upload-excel {
+        padding: 24px 8px;
     }
 
     .button {
@@ -167,45 +223,81 @@ Created by Kevin Le on 2021/3/4.
 
     .first-page {
         display: grid;
-        grid-template-columns: 50% 50%;
+        grid-template-columns: auto;
     }
-
-    .upload-file-block {
+    
+    .font-list {
+        margin-top: 24px;
+    }
+    
+    .font-list-item {
         position: relative;
-        margin: 12px;
-        padding: 24px 8px;
-        width: 50%;
-        border: #AAA 2px solid;
-        border-radius: 5px;
         display: grid;
-        grid-template-rows: 64px 24px;
-        text-align: center;
-        line-height: 24px;
-        cursor: pointer;
-        color: #AAA;
-    }
-
-    .upload-file-block.left {
-        align-self: flex-end;
-        justify-self: flex-end;
-    }
-
-    .upload-file-block.active,
-    .upload-file-block:hover {
+        grid-template-areas: 
+            "icon fontname fontname"
+            "icon filename upload-resultc";
+        grid-template-columns: 48px auto;
+        border: #A8A8A8 2px solid;
+        border-radius: 5px;
+        padding: 12px 24px;
         color: #383838;
+        margin-bottom: 8px;
+    }
+    
+    .font-list-item:hover {
+        background-color: #F8F8F8;
     }
 
-    .upload-file-block .material-icons {
-        line-height: 64px;
-        font-size: 64px;
+    .font-list-item-icon {
+        grid-area: icon;
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        background-color: #787878;
+        color: #FFFFFF;
+        line-height: 48px;
+        font-size: 32px;
+        text-align: center;
     }
-
-    .upload-file-block .file {
+    
+    .font-list-item-font-name {
+        grid-area: fontname;
+        padding-left: 12px;
+        color: #A8A8A8;
+    }
+    
+    .font-list-item-font-filename {
+        grid-area: filename;
+        padding-left: 12px;
+        color: #D8D8D8;
+    }
+    
+    .font-list-item-not-the-same-filename {
+        grid-area: upload-resultc;
+        text-align: end;
+        color: #FF8584;
+    }
+    
+    .font-list-item-file {
         position: absolute;
-        height: 100%;
+        top: 0;
+        left: 0;
         width: 100%;
+        height: 100%;
         opacity: 0;
-        cursor: pointer;
     }
-
+    
+    .font-list-item.active {
+        background: #383838;
+        border-color: #383838;
+        color: #FFFFFF;
+    }
+    
+    .font-list-item.active .font-list-item-font-name {
+        color: #FFFFFF;
+    }
+    
+    .font-list-item.active .font-list-item-font-filename {
+        color: #D8D8D8;
+    }
 </style>
