8748934b899b774c97b2f3d97ea535f35ecba0bb
kevinle <le8419951779@gmail.com>
2021-03-15 16:02:19 +0800

    [ReleaseFont] [Remove] 多餘的css

--- ReleaseFont/layout-main.vue
+++ ReleaseFont/layout-main.vue
@@ -204,39 +204,4 @@ Created by Kevin Le on 2021/3/4.
     .second-page {
         text-align: center;
     }
-
-    .loading {
-        display: inline-block;
-        width: 32px;
-        height: 32px;
-        border: 3px solid #00000000;
-        border-radius: 50%;
-        border-top: 3px solid #383838;
-        -webkit-animation: spin 2s linear infinite;
-        /* Safari */
-        animation: spin 2s linear infinite;
-        margin-top: 24px;
-    }
-
-    /* Safari */
-    @-webkit-keyframes spin {
-        0% {
-            -webkit-transform: rotate(0deg);
-        }
-
-        100% {
-            -webkit-transform: rotate(360deg);
-        }
-    }
-
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-
-        100% {
-            transform: rotate(360deg);
-        }
-    }
-
 </style>
