daef54dbfeb8441635146aebe951af542a09d720
kevinle <le8419951779@gmail.com>
2021-03-27 02:22:07 +0800

    [learn-go-frontend] [Add] Auth nav

---
+++ learn-go-frontend/src/components/auth/auth-nav.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="container">
+      <span id="login" :class="{active: is_login}" @click="onClick(true)">{{loginStr}}</span>
+      <span id="slash">|</span>
+      <span id="register" :class="{active: !is_login}" @click="onClick(false)">{{registerStr}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      is_login: {
+        type: Boolean,
+        default: false
+     }
+    }, 
+  data() {
+    return {
+      loginStr: "Login",
+      registerStr: "Register"
+    };
+  },
+  methods: {
+      onClick(isLogin) {
+          this.$emit('update:is_login', isLogin)
+      },
+  }
+};
+</script>
+
+<style scoped>
+#container {
+    height: 3rem;
+    line-height: 2rem;
+}
+
+#login, #register {
+    cursor: pointer;
+    color: #C8C8C8;
+    transition: all 1s;
+}
+
+#login:hover, #register:hover {
+    font-size: 1.4rem;
+    color: #787878;
+}
+
+#login.active, #register.active {
+    font-size: 2rem;
+    color: #383838;
+}
+
+#slash {
+    font-size: 2rem;
+    color: #C8C8C8;
+    padding: 0rem 0.5rem;
+}
+</style>
+
--- learn-go-frontend/src/pages/auth.vue
+++ learn-go-frontend/src/pages/auth.vue
@@ -1,17 +1,40 @@
 <template>
-    <div>
-        <h1>Auth</h1>
+  <div>
+    <h1>Auth</h1>
+    <div id="auth-container">
+      <AuthNav v-model:is_login="isLogin"/>
+      <input v-model="email" placeholder="Email" type="text" />
+      <input v-model="password" placeholder="Password" type="text" />
+      <input v-model="confirmPassword" v-if="!isLogin" placeholder="Confirm" type="text" />
+      <button>Login</button>
     </div>
+  </div>
 </template>
 
 <script>
+import AuthNav from '../components/auth/auth-nav.vue'
 export default {
-}
+  data() {
+    return {
+      isLogin: true,
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  },
+  components: {
+      AuthNav
+  },
+};
 </script>
 
 <style scoped>
-h1 {
-    color: #383838;
+#auth-container {
+  display: table;
+  margin: auto;
+  padding: 48px 24px;
+  border: 2px #383838 solid;
+  border-radius: 5px;
 }
 </style>
 
