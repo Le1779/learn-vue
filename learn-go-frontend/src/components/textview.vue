a272b76e18002d63f7fedca63cacffc3a083ed36
kevinle <le8419951779@gmail.com>
2021-03-27 03:33:30 +0800

    [learn-go-frontedn] [Add] textview

---
+++ learn-go-frontend/src/components/textview.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="container">
+    <input
+      v-model="val"
+      :placeholder="model.placeholder"
+      :type="model.type"
+      :name="model.name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value", "model"],
+  data() {
+    return {
+      val: this.value,
+    };
+  },
+  watch: {
+    val: function (val) {
+      this.$emit("update:value", val);
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+#container {
+  background: #e8e8e8;
+  height: 2.5rem;
+  border-radius: 1.25rem;
+  padding: 0 1.25rem;
+  line-height: 2.5rem;
+}
+
+input {
+  width: 100%;
+  background: transparent;
+  border: none;
+}
+
+input:focus {
+  outline: none;
+}
+
+input:-webkit-autofill {
+  transition: background-color 5000s ease-in-out 0s;
+}
+</style>
+
--- learn-go-frontend/src/pages/auth.vue
+++ learn-go-frontend/src/pages/auth.vue
@@ -2,28 +2,58 @@
   <div>
     <h1>Auth</h1>
     <div id="auth-container">
-      <AuthNav v-model:is_login="isLogin"/>
-      <input v-model="email" placeholder="Email" type="text" />
-      <input v-model="password" placeholder="Password" type="text" />
-      <input v-model="confirmPassword" v-if="!isLogin" placeholder="Confirm" type="text" />
+      <AuthNav id="auth-nav" v-model:is_login="isLogin" />
+      <Textview
+        class="textview"
+        v-model:value="email"
+        v-model:model="emailTextviewModel"
+      />
+      <Textview
+        class="textview"
+        v-model:value="password"
+        v-model:model="passwordTextviewModel"
+      />
+      <Textview
+        v-if="!isLogin"
+        class="textview"
+        v-model:value="confirmPassword"
+        v-model:model="confirmPasswordTextviewModel"
+      />
       <button>Login</button>
     </div>
   </div>
 </template>
 
 <script>
-import AuthNav from '../components/auth/auth-nav.vue'
+import AuthNav from "../components/auth/auth-nav.vue";
+import Textview from "../components/textview.vue";
 export default {
   data() {
     return {
       isLogin: true,
+      emailTextviewModel: {
+        placeholder: "Email",
+        type: "email",
+        name: "email",
+      },
+      passwordTextviewModel: {
+        placeholder: "Password",
+        type: "password",
+        name: "password",
+      },
+      confirmPasswordTextviewModel: {
+        placeholder: "Password",
+        type: "password",
+        name: "password",
+      },
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
     };
   },
   components: {
-      AuthNav
+    AuthNav,
+    Textview,
   },
 };
 </script>
@@ -35,6 +65,15 @@ export default {
   padding: 48px 24px;
   border: 2px #383838 solid;
   border-radius: 5px;
+  min-width: 360px;
+}
+
+#auth-nav {
+  margin-bottom: 2rem;
+}
+
+.textview {
+  margin-bottom: 1rem;
 }
 </style>
 
