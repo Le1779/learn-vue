1ec0f31f7951de132419579a7846b1bc9811c7b8
kevinle <le8419951779@gmail.com>
2021-04-16 17:45:23 +0800

    [Main][Add] add router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="header">
+    <h1>Learn Vue</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+    </div>
+  </div>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/pages/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #383838;
+}
+</style>
+
