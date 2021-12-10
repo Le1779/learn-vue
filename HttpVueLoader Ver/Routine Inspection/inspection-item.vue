53ab48b56904aa00294fc530fdf7555532ada3cf
kevinle <le8419951779@gmail.com>
2021-12-10 11:06:43 +0800

    [Add] (Routine Inspection) Type的select選項

--- HttpVueLoader Ver/Routine Inspection/inspection-item.vue
+++ HttpVueLoader Ver/Routine Inspection/inspection-item.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="item-container">
-    <div class="item-name-row">
+    <div class="item-row">
       <span>ID: </span>
       {{ item_data.ItemId }}
     </div>
-    <div class="item-name-row">
+    <div class="item-row">
       <span>Name: </span>
       <input v-model="item_data.Name" type="text" />
     </div>
-    <div class="item-name-row">
+    <div class="item-row">
       <span>Condition: </span>
       <input v-model="item_data.CheckCond" type="text" />
     </div>
-    <div class="item-name-row">
+    <div class="item-row">
       <span>Description: </span>
       <input v-model="item_data.Description" type="text" />
     </div>
-    <div class="item-name-row">
+    <div class="item-row">
       <span>Unit: </span>
       <input v-model="item_data.Unit" type="text" />
     </div>
-    <div class="item-name-row">
+    <div class="item-row">
       <span>Type: </span>
-      <select v-model="item_data.Type">
+      <select v-model="type">
         <option value="int">int</option>
         <option value="float(1)">float(1)</option>
         <option value="float(1)">float(2)</option>
         <option value="float(1)">float(3)</option>
         <option value="char(16)">char(16)</option>
+        <option value="select">select</option>
       </select>
+      <select v-model="singleOrMulti" v-if="type == 'select'">
+        <option value="S">單選</option>
+        <option value="M">多選</option>
+      </select>
+
+      <div class="item-row" v-if="type == 'select'">
+        <span>選項(請用,分割選項): </span>
+        <input v-model="selectOption" type="text" />
+      </div>
     </div>
     <div>
       <button class="delete-button" @click.stop="onDeleteButtonClick">
@@ -65,18 +75,64 @@ module.exports = {
     },
   },
 
-  data: () => ({}),
+  data: () => ({
+    /**
+     * 原始Type資料的暫存，因為select選項有特殊的格式。
+     */
+    type: null,
+    singleOrMulti: "S",
+    selectOption: "",
+  }),
+
+  watch: {
+    type: {
+      handler(val) {
+        if (val == "select") {
+          this.updateSelectType();
+          return;
+        }
+
+        this.item_data.Type = this.type;
+      },
+    },
+    singleOrMulti: {
+      handler(val) {
+        this.updateSelectType();
+      },
+    },
+    selectOption: {
+      handler(val) {
+        this.updateSelectType();
+      },
+    },
+  },
+
+  created() {
+    this.type = this.item_data.Type;
+  },
 
   methods: {
+    /**
+     * 點擊刪除按鈕，觸發父層的刪除動作。
+     */
     onDeleteButtonClick() {
       this.$emit("delete", this.index);
+    },
+
+    /**
+     * 更新單選or多選的Type
+     * [S]opt1,opt2
+     * [M]opt1,opt2
+     */
+    updateSelectType() {
+      this.item_data.Type = `[${this.singleOrMulti}]` + this.selectOption;
     },
   },
 };
 </script>
 
 <style scoped>
-.item-name-row {
+.item-row {
   padding: 8px 0;
 }
 </style>
